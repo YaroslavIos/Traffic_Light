0f20231f2aa297d80478074bf76dd628ad3dab43
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-04-20 21:17:44 +0300

    Initial Commit

---
+++ Traffic_Light/Traffic_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_LightApp.swift
+//  Traffic_Light
+//
+//  Created by Ярослав Любиченко on 20.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
