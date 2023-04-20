2c38e963ef358f931e1ee32bf7c553805eb2c288
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-04-21 00:06:40 +0300

    General settings. Creating and placing circles(set size and color). Add and set button(view and label). Code logic of app.

--- Traffic_Light/ContentView.swift
+++ Traffic_Light/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum TrafficLightColor {
+        case red
+        case yellow
+        case green
+    }
+    
+    @State var currentColor: TrafficLightColor = .red
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(.blue).ignoresSafeArea()
+            VStack {
+                Spacer()
+                
+                Circle()
+                    //.stroke(lineWidth: 5)
+                    .fill(currentColor == .red ?
+                        .red.opacity(1) : .red.opacity(0.1))
+                    .frame(width: 130, height: 130)
+                Circle()
+                    //.stroke(Color.yellow ,lineWidth: 2)
+                    .fill(currentColor == .yellow ?
+                        .yellow.opacity(1) : .yellow.opacity(0.1))
+                    .frame(width: 130, height: 130)
+                Circle()
+                    .fill(currentColor == .green ?
+                        .green.opacity(1) : .green.opacity(0.1))
+                    .frame(width: 130, height: 130)
+                
+                Spacer(minLength: 0)
+                
+                Button(action: {
+                    switch self.currentColor {
+                    case .red:
+                        self.currentColor = .yellow
+                    case .yellow:
+                        self.currentColor = .green
+                    case .green:
+                        self.currentColor = .red
+                    }
+                }, label: {
+                    Text("Switch Lights")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.black)
+                        .cornerRadius(16)
+                })
+            }// end vstack
+            .padding()
         }
-        .padding()
     }
 }
 
